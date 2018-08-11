6ed227e2e61d7142d4da6f9f88016676264a28de
Ishan Yapa <ishanyapa@gmail.com>
2018-08-12 01:54:27 +0530

    * POM updated. * Login UI

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,10 @@
+function login(email, password) {
+
+    console.log(email + " " + password)
+    console.log("function called");
+    if (email=="ishanyapa@gmail.com" && password=="123") {
+        return true;
+    }
+
+    return false;
+}
