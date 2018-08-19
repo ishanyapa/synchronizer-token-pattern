a856e4b0ffb7560269448e6cb22965141f5acad6
Ishan Yapa <ishanyapa@gmail.com>
2018-08-20 02:10:28 +0530

    Added Web content interceptor to remove caching

--- src/main/java/me/ishanyapa/stp/StpApplication.java
+++ src/main/java/me/ishanyapa/stp/StpApplication.java
@@ -2,8 +2,10 @@ package me.ishanyapa.stp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:WebContentInterceptor.xml")
 public class StpApplication {
 
 	public static void main(String[] args) {
