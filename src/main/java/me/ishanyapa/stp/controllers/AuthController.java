70e2382bbad91ded763c863af8c3995d3462b8cb
Ishan Yapa <ishanyapa@gmail.com>
2018-08-20 22:38:08 +0530

    Added a alert for login

--- src/main/java/me/ishanyapa/stp/controllers/AuthController.java
+++ src/main/java/me/ishanyapa/stp/controllers/AuthController.java
@@ -2,6 +2,7 @@ package me.ishanyapa.stp.controllers;
 
 import me.ishanyapa.stp.services.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class AuthController extends BaseController {
     @Autowired
     SessionService sessionService;
 
-    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(value = "/login")
     public void login(@RequestParam("email") String email, @RequestParam("password") String password, HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         if (String.valueOf(email).equals("ishanyapa@gmail.com") && String.valueOf(password).equals("123")) {
@@ -29,7 +30,7 @@ public class AuthController extends BaseController {
             return;
         }
 
-        response.sendRedirect("/index.html");
+        response.sendError(401);
     }
 
     @PostMapping(value = "/logout", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
