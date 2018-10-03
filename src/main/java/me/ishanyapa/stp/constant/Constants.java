9b41b6563fc93970a5d0f793c3f1e4e9d1a6e02c
Ishan Yapa <ishanyapa@gmail.com>
2018-10-03 23:37:13 +0530

    changed the username and password

--- src/main/java/me/ishanyapa/stp/constant/Constants.java
+++ src/main/java/me/ishanyapa/stp/constant/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
     public static final String USERNAME = "username";
-    public static final String EMAIL_VALUE = "ishanyapa@gmail.com";
-    public static final String PASSWORD_VALUE = "12345";
+    public static final String EMAIL_VALUE = "root";
+    public static final String PASSWORD_VALUE = "root";
     public static final String USERNAME_VALUE = "Ishan Yapa";
 }
